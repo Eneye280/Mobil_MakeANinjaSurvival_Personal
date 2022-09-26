eeb7cfb86ffdb0e5e5d81949386ff7cc5b1b312a
Euclides <eneye280@gmail.com>
2022-09-25 22:26:52 -0500

    DONE, Player Movement

---
+++ Assets/Make A Ninja Survival/Scripts/Player.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float movementAmplitude;
+    [SerializeField] private bool lockZ = false;
+
+    private Vector3 originalPosition;
+    private Vector3 targetPosition;
+    private Vector2 clickOrigin;
+
+    private void Start()
+    {
+        clickOrigin = Vector2.zero;
+    }
+
+    private void Update()
+    {
+        Vector2 viewportCoordinates = new Vector2
+        (
+            Input.mousePosition.x / Screen.width,
+            Input.mousePosition.y / Screen.height
+        );
+
+        if(Input.GetMouseButton(0))
+        {
+            if(clickOrigin == Vector2.zero)
+            {
+                originalPosition = transform.position;
+                clickOrigin = viewportCoordinates;
+            }
+            else
+            {
+                Vector2 variation = viewportCoordinates - clickOrigin;
+
+                targetPosition = new Vector3
+                (
+                    originalPosition.x + variation.x * movementAmplitude,
+                    transform.position.y,
+                    lockZ ? transform.position.z : originalPosition.z + variation.y * movementAmplitude
+                );
+            }
+        }
+        else
+        {
+            clickOrigin = Vector2.zero;
+        }
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, speed * Time.deltaTime);
+        transform.position = new Vector3(smoothPosition.x, transform.position.y, smoothPosition.z);
+    }
+}
